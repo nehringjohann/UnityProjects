113d4176c690bf664ee4fdf7302621eb281b7115
Johann Nehring Fritz <nehringjohann@hotmail.com>
2020-11-01 16:23:58 -0300

    Project Creation

---
+++ Assets/Scripts/InteractController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractController : MonoBehaviour
+{
+    public GameObject interactText;
+
+    private void OnTriggerStay(Collider otherObject)
+    {
+        if (otherObject.tag == "HitBox")
+        {
+            interactText.SetActive(true);
+            Debug.Log("Entered HitBox...");
+            if (Input.GetKey(KeyCode.E))
+            {
+                Debug.Log("Interacted with 'E'...");
+            }
+
+        }
+    }
+    
+    private void OnTriggerExit(Collider otherObject)
+    {
+        if (otherObject.tag == "HitBox")
+        {
+            interactText.SetActive(false);
+            Debug.Log("Left HitBox...");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform trans;
+    public Rigidbody rb;
+    public float movementSpeed = 50f;
+    public float runningSpeed = 1.5f;
+    public float jumpForce = 8f;
+
+    private bool isJumping = false;
+    
+    void Start()
+    {
+        trans = this.GetComponent<Transform>();
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float currentMovementSpeed = movementSpeed;
+        // Check if the Player is Running (Left Shift Key Pressed)
+        if (Input.GetKey(KeyCode.LeftShift)) { 
+            currentMovementSpeed = movementSpeed * runningSpeed;
+        }
+        // Move to the direction asked when Key is Pressed...
+        if (Input.GetKey(KeyCode.A)) {
+            trans.position -= trans.right * currentMovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            trans.position += trans.right * currentMovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            trans.position += trans.forward * currentMovementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            trans.position -= trans.forward * currentMovementSpeed * Time.deltaTime;
+        }
+        if (!isJumping)
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 up = transform.TransformDirection(Vector3.up);
+            rb.AddForce(up * jumpForce);
+        }
+    }
+
+    void OnCollisionEnter(Collision target)
+    {
+        isJumping = !target.gameObject.CompareTag("Ground");
+    }
+
+    void OnCollisionExit(Collision target)
+    {
+        isJumping = target.gameObject.CompareTag("Ground");
+    }
+}
